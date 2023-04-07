ea500d316c0f47a15b0bebc6f16df9d5357fb9a2
rob <rob@qwan.eu>
2023-04-07 09:35:28 +0200

    Ensure Coverate of findById in repository

--- src/test/java/eu/qwan/editrain/repositories/CourseRepositoryTests.java
+++ src/test/java/eu/qwan/editrain/repositories/CourseRepositoryTests.java
@@ -1,12 +1,14 @@
 package eu.qwan.editrain.repositories;
 
-import com.fasterxml.jackson.databind.deser.std.UUIDDeserializer;
 import eu.qwan.editrain.model.Course;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,14 +19,37 @@ import static org.hamcrest.Matchers.is;
 public class CourseRepositoryTests {
     @Autowired
     CourseRepository courseRepository;
-    @Test
-    public void isEmptyInitially() {
-        assertThat(courseRepository.findAll(), is(empty()));
+
+    @BeforeEach
+    public void setUp() {
+        courseRepository.deleteAll();
     }
-    @Test
-    public void containsSavedCourses() {
-        Course course = new Course(UUID.randomUUID().toString(), "name", "description", "john@edutrain.eu");
-        courseRepository.save(course);
-        assertThat(courseRepository.findAll(), is(List.of(course)));
+
+    @Nested
+    class ContentsOfRepository {
+        @Test
+        public void isEmptyInitially() {
+            assertThat(courseRepository.findAll(), is(empty()));
+        }
+
+        @Test
+        public void containsSavedCourses() {
+            Course course = new Course(UUID.randomUUID().toString(), "name", "description", "john@edutrain.eu");
+            courseRepository.save(course);
+            assertThat(courseRepository.findAll(), is(List.of(course)));
+        }
+    }
+    @Nested
+    class AccessingCoursesById {
+        @Test
+        public void returnsNothingWhenRepositoryIsEmpty() {
+            assertThat(courseRepository.findById("some-id"), is(Optional.empty()));
+        }
+        @Test
+        public void returnsASavedCourseWhenAvailable() {
+            Course course = new Course(UUID.randomUUID().toString(), "name", "description", "john@edutrain.eu");
+            courseRepository.save(course);
+            assertThat(courseRepository.findById(course.getId()), is(Optional.of(course)));
+        }
     }
 }
