6eee9eb14c70d65142863873448e30bfca733938
rob <rob@qwan.eu>
2023-04-07 09:29:46 +0200

    Removed autoconfiguration from CourseControllerTests

--- src/test/java/eu/qwan/editrain/controllers/CourseControllerTests.java
+++ src/test/java/eu/qwan/editrain/controllers/CourseControllerTests.java
@@ -7,11 +7,8 @@ import eu.qwan.editrain.model.EdiTrainException;
 import eu.qwan.editrain.services.CourseService;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,14 +20,10 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class CourseControllerTests {
-    @MockBean
-    private CourseService courseService;
+    private CourseService courseService = mock(CourseService.class);
 
-    @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(new CourseController(courseService)).build();
 
     @Nested
     public class GettingAllCourses {
