668ca314f58f12ff32a940c547a9ff9bbe46e346
rob <rob@qwan.eu>
2023-04-16 13:23:03 +0200

    used static build to improve syntax in CourseDtoMapperTests

--- src/test/java/eu/qwan/editrain/adapters/controllers/dto/CourseDtoMapperTests.java
+++ src/test/java/eu/qwan/editrain/adapters/controllers/dto/CourseDtoMapperTests.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static eu.qwan.editrain.adapters.controllers.dto.CourseDto.aValidCourseDto;
 import static eu.qwan.editrain.adapters.controllers.dto.CourseDtoMapper.*;
+import static eu.qwan.editrain.domain.builders.Builder.build;
 import static eu.qwan.editrain.domain.builders.CourseBuilder.aValidCourse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -18,7 +19,7 @@ public class CourseDtoMapperTests {
         @Test
         public void takesAllAttributesOverToCourse() {
             var dto = aValidCourseDto().id("some-id").name("some-name").description("some-description").teacher("some-teacher").build();
-            var course = aValidCourse().withId("some-id").withName("some-name").withDescription("some-description").taughtBy("some-teacher").build();
+            var course = build(aValidCourse().withId("some-id").withName("some-name").withDescription("some-description").taughtBy("some-teacher"));
             MatcherAssert.assertThat(toCourse(dto),equalTo(course));
         }
         @Test
@@ -35,14 +36,14 @@ public class CourseDtoMapperTests {
         @Test
         public void takesAllAttributesOverToCourse() {
             var record = aValidCourseDto().id("some-id").name("some-name").description("some-description").teacher("some-teacher").build();
-            var course = aValidCourse().withId("some-id").withName("some-name").withDescription("some-description").taughtBy("some-teacher").build();
+            var course = build(aValidCourse().withId("some-id").withName("some-name").withDescription("some-description").taughtBy("some-teacher"));
             assertThat(toCourseDto(course),equalTo(record));
 
         }
         @Test
         public void mappingAListMapsAllElementsInAList() {
-            var course1 = aValidCourse().withName("course1").build();
-            var course2 = aValidCourse().withName("course2").build();
+            var course1 = build(aValidCourse().withName("course1"));
+            var course2 = build(aValidCourse().withName("course2"));
             assertThat(toCourseDtoList(List.of(course1, course2)),
                     equalTo(List.of(toCourseDto(course1), toCourseDto(course2))));
 
